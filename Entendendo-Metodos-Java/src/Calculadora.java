235abf9f5e19794f4a727c1a79c52f15adf0bde2
LuizBorgesss <luizeduardobrgs@gmail.com>
2023-01-17 18:36:35 -0300

    Exercicios Métodos

---
+++ Entendendo-Metodos-Java/src/Calculadora.java
@@ -0,0 +1,33 @@
+
+
+public class Calculadora {
+    public static void soma(double numero1, double numero2) {
+    
+        double resultado = numero1 + numero2;
+
+        System.out.println("A soma do " + numero1 + " mais " + numero2 + " é = " + resultado);
+    } 
+
+    public static void subtracao(double numero1, double numero2) {
+        
+        double resultado = numero1 - numero2;
+
+        System.out.println("A subtração do " + numero1 + " menos " + numero2 + " é = " + resultado);
+    } 
+
+    public static void divisao(double numero1, double numero2) {
+        
+        double resultado = numero1 / numero2;
+
+        System.out.println("A divisão do " + numero1 + " pelo " + numero2 + " é = " + resultado);
+    } 
+
+    public static void multiplicacao(double numero1, double numero2) {
+        
+        double resultado = numero1 * numero2;
+
+        System.out.println("A multiplicacao do " + numero1 + " vezes " + numero2 + " é = " + resultado);
+    } 
+    
+}
+
---
+++ Entendendo-Metodos-Java/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Método calculadora
+        System.out.println("EXERCICIO CALCULADORA");
+        Calculadora.soma(10, 30);
+        Calculadora.subtracao(15, 10);
+        Calculadora.divisao(20, 5);
+        Calculadora.multiplicacao(50, 100);
+        
+        // Método Obter mensagem
+        System.out.println("EXERCICIO MENSAGEM");
+        Mensagem.obterMensagem(6);
+        Mensagem.obterMensagem(15);
+        Mensagem.obterMensagem(22);
+    
+        // Método Emprestimo
+        System.out.println("EXERCICIO EMPRÉSTIMO");
+        Emprestimo.calcular(10000, 3);
+    }
+
+}
